f4fc4709e70b4e6e2857a68c6f17923122b6c107
codecakes <akurnya@gmail.com>
2014-11-21 15:59:30 +0400

    raked db with correct method names for each table col type. not capital

--- db/migrate/20141121103859_create_movies.rb
+++ db/migrate/20141121103859_create_movies.rb
@@ -1,10 +1,10 @@
 class CreateMovies < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.String :title
-      t.Integer :rating
-      t.Text :description
-      t.Datetime :release_date
+      t.string :title
+      t.integer :rating
+      t.text :description
+      t.datetime :release_date
 
       t.timestamps
     end
